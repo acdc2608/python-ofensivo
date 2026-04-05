72f7007ae055508a7352ac8196d29b2d8e4459a9
acdc2608 <acdc2808@proton.me>
2026-04-05 10:14:53 -0600

    🐍 String formatting

---
+++ bases/str_formatting.py
@@ -0,0 +1,11 @@
+
+name = "acdc2608"
+programming_language = "Python"
+edad = 25
+
+print("Hola, mi usuario es: %s y programo en: %s. Actualmente tengo %d" % (name, programming_language, edad))
+
+print("Hola mundo, soy {} y tengo {} años!".format(name, edad))
+print("Hola mundo, soy {0} y tengo {1} años!. Me gusta que me digan {0}".format(name, edad))
+
+print(f"Hola mi ususario es: {name}, tengo {edad} y programo en {programming_language}")
