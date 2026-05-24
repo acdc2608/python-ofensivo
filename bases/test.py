4564d0055ce3750247593deb55b2056afd3f2111
acdc2608 <acdc2808@proton.me>
2026-05-24 09:24:36 -0600

    funciones@

---
+++ bases/test.py
@@ -0,0 +1,23 @@
+print(tuple([1,2,3]))
+
+[2,3,41,2,1].remove(2)
+
+dict1 = {
+    "nombre": "Alexis"
+}
+dict2 = {
+    "nombre": "Alexis"
+}
+
+dict1.update(dict2)
+
+print(dict1)
+
+var1 = (1,)
+print(var1)
+
+
+conjunto = set([1,2,2,3])
+conjunto2 = {12,35,2}
+print(conjunto)
+print(conjunto2)
